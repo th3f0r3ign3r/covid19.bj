30e39e21bb44f77e29480f4b574ed527138749ff
th3f0r3ign3r <7h3.f0r319n3r@gmail.com>
2020-10-13 12:51:58 +0100

    Optimisation & signature add

--- covid19.py
+++ covid19.py
@@ -5,12 +5,10 @@ import sys,requests
 
 class cli_colors:
     header = '\033[95m'
-    okblue = '\033[94m'
     okgreen = '\033[92m'
     warning = '\033[93m'
     fail = '\033[91m'
     endc = '\033[0m'
-    bold = '\033[1m'
     underline = '\033[4m'
 
 def sysout(output=''):
@@ -18,12 +16,14 @@ def sysout(output=''):
 
 gouvbjpage = requests.get('http://www.gouv.bj/coronavirus/')
 soup = BeautifulSoup(gouvbjpage.content,'html.parser')
-a = soup.find_all('h2',{'class': ['h1 adapt white left-5']})
+h2 = soup.find_all('h2',{'class': ['h1 adapt white left-5']})
 
 sysout(soup.find('title').text)
 sysout()
-sysout(cli_colors.fail + 'Cas confirmés\t:' + " " + a[0].text)
-sysout(cli_colors.warning + 'Sous traitement\t:' + " " + a[1].text)
-sysout(cli_colors.okgreen + 'Cas guéris\t:' + " " + a[2].text)
-sysout(cli_colors.header + 'Décès\t:' + " " + a[3].text)
+sysout(cli_colors.fail + 'Cas confirmés\t:' + " " + h2[0].text)
+sysout(cli_colors.warning + 'Sous traitement\t:' + " " + h2[1].text)
+sysout(cli_colors.okgreen + 'Cas guéris\t:' + " " + h2[2].text)
+sysout(cli_colors.header + 'Décès\t:' + " " + h2[3].text)
+sysout()
+sysout(cli_colors.underline + 'by th3f0r3ign3r')
 sysout()
